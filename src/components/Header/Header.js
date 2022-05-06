03f0fd85bffc0190a252fd749ba1631147eeb951
HoangLong <hoanglong15022k@gmail.com>
2022-05-06 23:11:10 +0700

    fix tam search

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,9 @@
 import { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import apiMovie from "../../api/axios";
-import requests from "../../FecthDataMovie/FecthDataAll";
 import { APIKey } from "../../api/apikey";
 import "firebase/compat/auth";
 import firebase from "firebase/compat/app";
-import { LoadingContext } from "../../Context/LoadingContext";
 import ReactLoading from "react-loading";
 import "./Header.css";
 function Header({ isSignedIn }) {
@@ -15,28 +13,27 @@ function Header({ isSignedIn }) {
   //  //code
   // }
   // console.log(logo)
-
-  const context = useContext(LoadingContext);
+  const [isLoading, setIsLoading] = useState(false);
   const [movies, setMovies] = useState([]);
   const [keywords, setKeyWords] = useState("");
   // console.log(context.isLoading)
   useEffect(() => {
-    // context.setIsLoading(true);
     const fetchMovie = async () => {
       try {
         const { data } = await apiMovie.get(
           `/search/movie?api_key=${APIKey}&language=vi&page=1&include_adult=false&query=${keywords}`
         );
         setMovies(data && data.results);
-        // setTimeout(() => {
-        //   context.setIsLoading(false);
-        // }, 700);
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 1000);
         // console.log(data && data.results);
       } catch (error) {
         console.error(error);
       }
     };
     if (keywords !== "") {
+      setIsLoading(true);
       fetchMovie();
     }
   }, [keywords]);
@@ -67,51 +64,52 @@ function Header({ isSignedIn }) {
             <span>Năm phát hành</span>
           </div>
           <div className="search">
-            <input
-              value={keywords}
-              onChange={(e) => {
-                setKeyWords(e.target.value);
-                if (keywords !== "") {
-                  setMovies([]);
-                }
-              }}
-              type="text"
-              placeholder="Search..."
-            ></input>
-            {console.log(keywords)}
+            <div className="input-search">
+              <input
+                value={keywords}
+                onChange={(e) => {
+                  setKeyWords(e.target.value);
+                  if (keywords !== "") {
+                    setMovies([]);
+                  }
+                }}
+                type="text"
+                placeholder="Search..."
+              ></input>{" "}
+            </div>
             <div className="search-results">
-              {/* {context.isLoading ? (
-                <div className="isloading">
+              {isLoading ? (
+                <div className="isloading-search">
                   <ReactLoading
                     type="bubbles"
                     color={"black"}
                     className="loading"
                   />
                 </div>
-              ) : ( */}
-              <>
-                {movies.slice(0, 7).map((movie, index) => (
-                  <Link
-                    className="search-results-item"
-                    to={`/movie/${movie.id}`}
-                    key={index}
-                  >
-                    <img
-                      style={{ width: "10%" }}
-                      src={
-                        movie.poster_path
-                          ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-                          : "http://hoahieu.com.vn/wp-content/themes/kutetheme/images/placeholder.jpg"
-                      }
-                      alt="Poster"
-                    />
-                    <div className="search-results-title">
-                      {movie.original_title}
-                    </div>
-                  </Link>
-                ))}
-              </>
-              {/* )} */}
+              ) : (
+                <>
+                  {movies.slice(0, 7).map((movie, index) => (
+                    <Link
+                      className="search-results-item"
+                      to={`/movie/${movie.id}`}
+                      key={index}
+                    >
+                      <img
+                        style={{ width: "10%" }}
+                        src={
+                          movie.poster_path
+                            ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                            : "http://hoahieu.com.vn/wp-content/themes/kutetheme/images/placeholder.jpg"
+                        }
+                        alt="Poster"
+                      />
+                      <div className="search-results-title">
+                        {movie.original_title}
+                      </div>
+                    </Link>
+                  ))}
+                </>
+              )}
             </div>
           </div>
         </div>
